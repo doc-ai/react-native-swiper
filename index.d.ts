ca74428b8dd0a19e1f51663f403d82a1b2758753
odoland <orlando.wong@yahoo.com>
2020-07-11 16:00:01 -0700

    Update declarions again

--- index.d.ts
+++ index.d.ts
@@ -103,6 +103,5 @@ declare module 'react-native-swiper' {
 
     export default class Swiper extends Component<SwiperProps, any> {
         scrollBy: (index?: number, animated?: boolean) => void
-        scrollTo: (index: number, animated?: boolean) => void
     }
 }
